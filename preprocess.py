acada840e4dd35a6584bc16b75052e4d6ec7175a
kwong60 <kara_wong@brown.edu>
2024-04-25 13:18:28 -0400

    separate train and test

--- preprocess.py
+++ preprocess.py
@@ -13,35 +13,46 @@ def load_data(data_folder):
     Feel free to ignore this, but please read over this if you want a little more clairity 
     on how the images and captions were pre-processed 
     '''
-    if f'{data_folder}' == "test":
-        text_file_path = f'{data_folder}/test_labels.txt'
-    if f'{data_folder}' == "train":
-        text_file_path = f'{data_folder}/train_labels.txt'
+    
+    test_fle_path = f'{data_folder}/test_labels.txt'
+    train_file_path = f'{data_folder}/train_labels.txt'
 
-    with open(text_file_path) as file:
-        examples = file.read().splitlines()[1:]
+    with open(test_fle_path) as file:
+        test_examples = file.read().splitlines()[1:]
+        
+    with open(train_file_path) as file:
+        train_examples = file.read().splitlines()[1:]
     
     # map each image name to its captions
-    image_names_to_labels = {}
-    for example in examples:
-        img_name, label = example.split(',', 1)
-        image_names_to_labels[img_name] = image_names_to_labels.get(img_name, []) + [label]
+    test_image_names_to_labels = {}
+    for test_example in test_examples:
+        test_img_name, test_label = test_example.split(',', 1)
+        test_image_names_to_labels[test_img_name] = test_image_names_to_labels.get(test_img_name, []) + [test_label]
+        
+    train_image_names_to_labels = {}
+    for train_example in train_examples:
+        train_img_name, train_label = train_example.split(',', 1)
+        train_image_names_to_labels[train_img_name] = train_image_names_to_labels.get(train_img_name, []) + [train_label]
 
-    images = list(image_names_to_labels.keys())
+    test_images = list(test_image_names_to_labels.keys())
+    train_images = list(train_image_names_to_labels.keys())
 
     # returns all captions for all images
-    def get_all_labels(image_names):
+    def get_all_labels(image_names, image_names_to_labels):
         to_return = []
         for image in image_names:
             label = image_names_to_labels[image]
             to_return.append(label)
         return to_return
     
-    labels = get_all_labels(images)
+    test_labels = get_all_labels(test_images, test_image_names_to_labels)
+    train_labels = get_all_labels(train_images, train_image_names_to_labels)
 
     return dict(
-        labels          = np.array(labels),
-        images            = np.array(images),
+        test_labels          = np.array(test_labels),
+        test_images            = np.array(test_images),
+        train_labels          = np.array(train_labels),
+        train_images            = np.array(train_images),
     )
 
 
@@ -91,23 +102,23 @@ def get_data(file_path, first_class, second_class):
 	inputs = unpickled_file[b'images']
 	labels = unpickled_file[b'labels']
 	# Extract inputs and labels corresponding to the two classes
-	extracted_indices = np.ones(len(labels))
-	for i in range(len(labels)):
-		label = labels[i]
-		if label != first_class and label != second_class:
-			extracted_indices[i] = 0
-	inputs = inputs[np.nonzero(extracted_indices)]
-	labels = np.array(labels)
-	labels = labels[np.nonzero(extracted_indices)]
-	# Reshape and transpose the inputs
-	inputs = tf.reshape(inputs, (-1, 3, 32 ,32))
-	inputs = tf.transpose(inputs, perm=[0,2,3,1]) # Now in the shape (num_examples, 32, 32, 3)
-	# Re-number the labels such that the cat label class is 0 and the dog label is 1
-	labels = np.where(labels == first_class, 0, 1)
-	# Turn your labels into one-hot vectors
-	labels = tf.one_hot(labels, 2)
-	# Normalize the inputs
-	inputs = inputs / 255
+	# extracted_indices = np.ones(len(labels))
+	# for i in range(len(labels)):
+	# 	label = labels[i]
+	# 	if label != first_class and label != second_class:
+	# 		extracted_indices[i] = 0
+	# inputs = inputs[np.nonzero(extracted_indices)]
+	# labels = np.array(labels)
+	# labels = labels[np.nonzero(extracted_indices)]
+	# # Reshape and transpose the inputs
+	# inputs = tf.reshape(inputs, (-1, 3, 32 ,32))
+	# inputs = tf.transpose(inputs, perm=[0,2,3,1]) # Now in the shape (num_examples, 32, 32, 3)
+	# # Re-number the labels such that the cat label class is 0 and the dog label is 1
+	# labels = np.where(labels == first_class, 0, 1)
+	# # Turn your labels into one-hot vectors
+	# labels = tf.one_hot(labels, 2)
+	# # Normalize the inputs
+	# inputs = inputs / 255
 	return inputs, labels
 
 
