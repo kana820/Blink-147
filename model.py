00d7a4fc0301a075d9f6a2c85fb98bb2e53bd4dc
kwong60 <kara_wong@brown.edu>
2024-05-04 00:36:19 -0400

    cleaned preprocess + BTL

--- model.py
+++ model.py
@@ -4,9 +4,8 @@ from tensorflow import keras
 import numpy as np
 import random
 import math
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from PIL import Image
-from sklearn.metrics import confusion_matrix
 
 class AttentionBlock(tf.keras.layers.Layer):
     def __init__(self, normalize_attn=True):
@@ -15,7 +14,14 @@ class AttentionBlock(tf.keras.layers.Layer):
         self.op = tf.keras.layers.Conv2D(filters=1, kernel_size=1, padding='valid', use_bias=False)
 
     def call(self, l, g):
-        N, W, H, C = tf.shape(l)
+        # N, W, H, C = tf.shape(l)
+
+        N, W, H, C = tf.split(tf.shape(l), num_or_size_splits=4)
+        N = tf.squeeze(N, axis=0)
+        W = tf.squeeze(W, axis=0)
+        H = tf.squeeze(H, axis=0)
+        C = tf.squeeze(C, axis=0)
+
         c = self.op(tf.concat([l, g], axis=-1))  # batch_sizexWxHx1
         c = tf.squeeze(c, axis=-1)
 
@@ -29,7 +35,6 @@ class AttentionBlock(tf.keras.layers.Layer):
         g = tf.multiply(a, l)
         if self.normalize_attn:
             g = tf.reduce_sum(g, axis=[1, 2])  # batch_sizexC
-
         else:
             g = tf.reduce_mean(g, axis=[1, 2], keepdims=True)  # batch_sizex1x1xC
             g = tf.squeeze(g, axis=[1, 2])  # batch_sizexC
@@ -40,12 +45,10 @@ class ProjectorBlock(tf.keras.layers.Layer):
     def __init__(self, out_size):
         super(ProjectorBlock, self).__init__()
         self.out_size = out_size
-        # self.conv = tf.keras.layers.Conv2D(filters=out_features, kernel_size=4, padding="SAME")
         
     def call(self, inputs):
-        resized_image = tf.image.resize(inputs, size=(self.out_size, self.out_size))
-        return resized_image
-        # return self.conv(resized_image)
+        # return self.op(inputs)
+        return tf.image.resize(inputs, size=(self.out_size, self.out_size))
 
 
 class Model(tf.keras.Model):
@@ -53,24 +56,36 @@ class Model(tf.keras.Model):
         super(Model, self).__init__()
         
         self.num_epoch = num_epoch
-        self.conv1 = tf.keras.layers.Conv2D(filters=32, strides=(2, 2), kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
-        self.conv2 = tf.keras.layers.Conv2D(filters=64, strides=(2, 2), kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
-        self.conv3 = tf.keras.layers.Conv2D(filters=128, kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
-        self.conv4 = tf.keras.layers.Conv2D(filters=128,kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
+
+        self.conv1 = tf.keras.layers.Conv2D(filters=64, kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU(), input_shape=(100,100,3))
+        self.conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
+        self.conv3 = tf.keras.layers.Conv2D(filters=64,kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
+        self.conv4 = tf.keras.layers.Conv2D(filters=64,kernel_size=kernel_size, padding="SAME", activation=tf.keras.layers.LeakyReLU())
         self.maxpool = tf.keras.layers.MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="SAME")
+
+        # self.dense = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=int(im_size/32), padding=0, bias=True)
+
+        # self.flatten = tf.keras.layers.Flatten()
 
         self.dense1_classification  = tf.keras.layers.Dense(64, activation=tf.keras.layers.LeakyReLU())
         self.dense2_classification  = tf.keras.layers.Dense(32, activation=tf.keras.layers.LeakyReLU())
         self.dense3_classification  = tf.keras.layers.Dense(4)
 
         self.loss_list = []
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.dropout_rate = 0.15
-        self.dropout = tf.keras.layers.Dropout(self.dropout_rate) 
+        self.epoch_loss = []
+        self.acc_list = []
+        self.epoch_acc = []
+        self.test_acc = []
+        self.optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.001)
 
-        self.projector1 = ProjectorBlock(25)
-        self.projector2 = ProjectorBlock(7)
-        self.projector3 = ProjectorBlock(4)
+        self.dropout_rate = 0.10
+        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
+
+
+        # self.projector = ProjectorBlock(7)
+        self.projector1 = ProjectorBlock(50)
+        self.projector2 = ProjectorBlock(25)
+        self.projector3 = ProjectorBlock(13)
         self.attn1 = AttentionBlock()
         self.attn2 = AttentionBlock()
         self.attn3 = AttentionBlock()
@@ -82,22 +97,21 @@ class Model(tf.keras.Model):
         p1 = self.maxpool(conv1)
         if training: p1 = self.dropout(p1)
         else: p1 = p1 * (1 - self.dropout_rate)
-        # print(p1.shape) # (256, 25, 25, 32)
+
         conv2 = self.conv2(p1)
         p2 = self.maxpool(conv2)
         if training: p2 = self.dropout(p2)
         else: p2 = p2 * (1 - self.dropout_rate)
-        # print(p2.shape) # (256, 7, 7, 64)
+
         conv3 = self.conv3(p2)
         p3 = self.maxpool(conv3)
         if training: p3 = self.dropout(p3)
         else: p3 = p3 * (1 - self.dropout_rate)
-        # print(p3.shape) # (256, 4, 4, 128)
+
         conv4 = self.conv4(p3)
         p4 = self.maxpool(conv4)
         if training: p4 = self.dropout(p4)
         else: p4 = p4 * (1 - self.dropout_rate)
-        # print(p4.shape) # (256, 2, 2, 128)
 
         c1, g1 = self.attn1(p1, self.projector1(p4))
         c2, g2 = self.attn2(p2, self.projector2(p4))
@@ -107,32 +121,31 @@ class Model(tf.keras.Model):
         g = self.layer_norm(g)
     
         d1 = self.dense1_classification(g)
-        #d1 = tf.nn.dropout(d1,0.1)
         d2 = self.dense2_classification(d1)
-        #d2 = tf.nn.dropout(d2,0.1)
         d3 = self.dense3_classification(d2)
         return d3
     
     def loss(self, logits, labels):
-        # class_weights = tf.constant([[6.749, 6.443, 11.781, 1.628]])
-        # class_weights = tf.constant([[0.25, 0.24, 0.435, 0.065]])
-        # class_weights = tf.constant([[1, 1, 1.2, 0.9]])
-        
-        # weight_per_label = tf.transpose(tf.matmul(labels, tf.transpose(class_weights)))
-        # loss = weight_per_label * tf.nn.softmax_cross_entropy_with_logits(labels, logits)
+        class_weights = tf.constant([[6.749, 6.443, 11.781, 1.628]])
 
-        loss = tf.nn.softmax_cross_entropy_with_logits(labels,logits)
+        # weights = tf.constant([class_weights[i] for i in labels])
+        weight_per_label = tf.transpose(tf.matmul(labels, tf.transpose(class_weights)))
+
+        # loss = tf.nn.softmax_cross_entropy_with_logits(labels,logits)
+
+        loss = weight_per_label * tf.nn.softmax_cross_entropy_with_logits(labels, logits)
+
         return tf.reduce_mean(loss)
     
     def accuracy(self, logits, labels):
-        
         x = tf.nn.softmax(logits)
         correct_predictions = tf.equal(tf.argmax(x, 1), tf.argmax(labels, 1))
-        # preds = tf.argmax(x, 1).numpy()
-        # print("blink", len([pred for pred in preds if pred == 3]))
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
     
-
+    def model(self):
+        x = tf.keras.layers.Input(shape=(100, 100, 3))
+        return tf.keras.Model(inputs=[x], outputs=self.call(x, training=True))
+    
     def visualize_cnn_layer(self, img, nrows, ncols, figsize, view_img=True):
         '''
         Not working yet
@@ -156,3 +169,43 @@ class Model(tf.keras.Model):
                     curr_ax.imshow(img, alpha=0.3)
 
         return fig, axes
+    
+    def visualize_attention(self, img):
+        # image
+        img = np.array(img)  / 255
+
+        conv1_out = self.maxpool(self.conv1(img[None,:,:,:]))
+        conv2_out = self.maxpool(self.conv2(conv1_out))
+        conv3_out = self.maxpool(self.conv3(conv2_out))
+        conv4_out = self.maxpool(self.conv4(conv3_out))
+
+        print(conv4_out.shape)
+
+        c1, g1 = self.attn1(conv1_out, self.projector1(conv4_out))
+        c2, g2 = self.attn1(conv2_out, self.projector2(conv4_out))
+        c3, g3 = self.attn1(conv3_out, self.projector3(conv4_out))
+
+        out1 = np.squeeze(np.array(c1.numpy() * 255).astype(np.uint8))
+        out1 = Image.fromarray(out1)
+        out1 = out1.resize((100, 100), resample=Image.BOX)
+
+        out2 = np.squeeze(np.array(c2.numpy() * 255).astype(np.uint8))
+        out2 = Image.fromarray(out2)
+        out2 = out2.resize((100, 100), resample=Image.BOX)
+
+        out3 = np.squeeze(np.array(c3.numpy() * 255).astype(np.uint8))
+        out3 = Image.fromarray(out3)
+        out3 = out2.resize((100, 100), resample=Image.BOX)
+
+        outs = [out1, out2, out3]
+
+        fig, axs = plt.subplots(1, 3)
+        for col in range(3):
+            ax = axs[col]
+            curr_plot = outs[col]
+            pcm = ax.imshow(curr_plot)
+            ax.imshow(img, alpha=0.85)
+            plot_title = "Attention Map " + str(col)
+            ax.set_title(plot_title)
+        fig.colorbar(pcm, ax=axs[:], shrink=0.4)
+        plt.show()
